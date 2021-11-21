31d189241c398ba82e61d8161ae863658586392f
AkshithPOttigari <akshithpottigari@gmail.com>
2021-11-21 20:15:48 +0530

    learnt getelementbyid and getelementbyClass

--- 1/1.js
+++ 1/1.js
@@ -1,8 +1,19 @@
+//getelementbyID
+
 function colorchange(){
     let color = document.getElementById("demo")
     color.style.color = "red"
 }
+
 function BackgroundColorChange(){
-    let color = document.getElementById("BackgroundColorChange")
+    let color = document.body.style.backgroundColor = "green"
 color.style.backgroundColor = "black"
+}
+
+// getelementbyclass
+
+function ChangeListtext(){
+    let List = document.getElementsByClassName("Lists")
+    List[0].textContent = "Juice"
+    List[0].style.color = "Magenta"
 }
